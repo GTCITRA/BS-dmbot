c552fbefcfa83f1ec3c8725424ba9cecf16ed8ed
GTCITRA <70244453+GTCITRA@users.noreply.github.com>
2020-08-26 10:03:20 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -12,7 +12,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇이 정상적으로 실행되었습니다.")
-    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    game = discord.Game('트라 DM봇')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #/dm {할말}로 전체DM 전송
@@ -28,11 +28,11 @@ async def on_message(message):
                     #메시지 관리권한이 있을시 사용가능
                     if message.author.guild_permissions.manage_messages:
                         embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at)
-                        embed.add_field(name="★★제목★★", value=msg, inline=True)
-                        embed.set_footer(text=f"discord.gg/★★서버초대코드★★")
+                        embed.add_field(name="트라 DM봇", value=msg, inline=True)
+                        embed.set_footer(text=f"discord.gg/KuJP7R2")
                         await i.send(embed=embed)
                 except:
                     pass
 
 
-client.run('★★봇토큰★★')
+client.run('NzQ3OTgxMjE0NTQ3MTgxNjAw.X0Wxyg.HDMrS5t0u63yWsAVJ3eo7MWTfjk')
